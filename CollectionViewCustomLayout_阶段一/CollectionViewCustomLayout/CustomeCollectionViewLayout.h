26b05dae725421ac0c651657247b2c12f312c4f8
zeluliCredit <zeluli@creditease.cn>
2015-09-22 13:59:02 +0800

    自定义布局阶段一代码提交

---
+++ CollectionViewCustomLayout_阶段一/CollectionViewCustomLayout/CustomeCollectionViewLayout.h
@@ -0,0 +1,13 @@
+//
+//  CustomeCollectionViewLayout.h
+//  CollectionViewCustomLayout
+//
+//  Created by Mr.LuDashi on 15/9/15.
+//  Copyright (c) 2015年 ZeluLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomeCollectionViewLayout : UICollectionViewLayout
+
+@end
---
+++ CollectionViewCustomLayout_阶段一/CollectionViewCustomLayout/MyCollectionViewController.m
@@ -0,0 +1,105 @@
+//
+//  MyCollectionViewController.m
+//  CollectionViewCustomLayout
+//
+//  Created by Mr.LuDashi on 15/9/15.
+//  Copyright (c) 2015年 ZeluLi. All rights reserved.
+//
+
+#import "MyCollectionViewController.h"
+#import "ImageCollectionViewCell.h"
+#import "CustomeCollectionViewLayout.h"
+
+@interface MyCollectionViewController ()
+
+@end
+
+@implementation MyCollectionViewController
+
+static NSString * const reuseIdentifier = @"Cell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)tapRefrash:(id)sender {
+    [self.collectionView reloadData];
+}
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 500;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    ImageCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
+    NSInteger imageIndex = arc4random()%10;
+    
+    NSString *imageName = [NSString stringWithFormat:@"00%ld.jpg", imageIndex];
+    
+    [cell.cellImageView setImage:[UIImage imageNamed:imageName]];
+    
+    // Configure the cell
+    
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+- (UICollectionViewTransitionLayout *)collectionView:(UICollectionView *)collectionView transitionLayoutForOldLayout:(UICollectionViewLayout *)fromLayout newLayout:(UICollectionViewLayout *)toLayout{
+    
+    UICollectionViewTransitionLayout *transition = [[UICollectionViewTransitionLayout alloc] initWithCurrentLayout:fromLayout nextLayout:toLayout];
+    return transition;
+}
+
+/*
+// Uncomment this method to specify if the specified item should be highlighted during tracking
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
+}
+*/
+
+/*
+// Uncomment this method to specify if the specified item should be selected
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+*/
+
+/*
+// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	return NO;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	
+}
+*/
+
+@end
